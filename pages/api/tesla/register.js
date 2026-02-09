fd08d8b9670f4c69b0308fcf457f3201d341c753
topbilling <43326946+topbilling@users.noreply.github.com>
2026-02-08 18:59:19 -0700

    Update register.js

--- pages/api/tesla/register.js
+++ pages/api/tesla/register.js
@@ -1,13 +1,16 @@
 export default async function handler(req, res) {
   const { token } = req.query;
 
+  // We are specifically targeting the North American server
   const response = await fetch('https://fleet-api.prd.na.vn.cloud.tesla.com/api/1/partner/register', {
     method: 'POST',
     headers: {
       'Authorization': `Bearer ${token}`,
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({}) // Tesla requires an empty object
+    body: JSON.stringify({
+      "domain": "ph-energy-dash.vercel.app" // Adding your specific domain
+    })
   });
 
   const data = await response.json();
