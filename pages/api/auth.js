021269fe0786b68612ad66d7a0b59d6e32499aaa
topbilling <43326946+topbilling@users.noreply.github.com>
2026-02-09 13:17:13 -0700

    Update auth.js

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,1 +1,20 @@
+export default async function handler(req, res) {
+  const { code } = req.query;
 
+  const response = await fetch('https://fleet-auth.prd.vn.cloud.tesla.com/oauth2/v3/token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams({
+      grant_type: 'authorization_code',
+      client_id: process.env.TESLA_CLIENT_ID,
+      client_secret: process.env.TESLA_CLIENT_SECRET,
+      code,
+      redirect_uri: 'https://ph-energy-dash.vercel.app/api/tesla/auth',
+      audience: 'https://fleet-api.prd.na.vn.cloud.tesla.com'
+    })
+  });
+
+  const data = await response.json();
+  // This will display your tokens on the screen so you can save them
+  res.status(200).json(data);
+}
