7dc85276c803d64e201eee202bc7328d9bec2a05
topbilling <43326946+topbilling@users.noreply.github.com>
2026-02-09 13:17:40 -0700

    Update live-status.js

--- pages/api/live-status.js
+++ pages/api/live-status.js
@@ -1,1 +1,11 @@
+export default async function handler(req, res) {
+  const { token } = req.query; // Use the User Access Token here
+  const siteId = process.env.TESLA_ENERGY_SITE_ID;
 
+  const response = await fetch(`https://fleet-api.prd.na.vn.cloud.tesla.com/api/1/energy_sites/${siteId}/live_status`, {
+    headers: { 'Authorization': `Bearer ${token}` }
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
